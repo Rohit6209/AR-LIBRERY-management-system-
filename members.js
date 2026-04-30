9159803681f858320ec85c4d6c716a1631d832f6
Rohit6209 <152963514+Rohit6209@users.noreply.github.com>
2026-04-30 11:54:57 +0530

    Fix import path for verifyAdmin middleware

--- members.js
+++ members.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router  = express.Router();
 const db      = require('./config/db');
-const { verifyAdmin } = require('../middleware/auth');
+const { verifyAdmin } = require('./middleware/auth');
 
 // GET all members
 router.get('/', async (req, res) => {
